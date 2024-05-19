4663751c183c39810a041ba265f3b418c1303f0e
skykwon1004 <skykwon1004@gmail.com>
2024-05-19 17:02:54 +0900

    체크박스기능과 코딩애플강의

--- detail.js
+++ detail.js
@@ -12,24 +12,48 @@
 //     })
 // }
 
-let length = $('.tab-button').length
-console.log(length)
+let length = $(".tab-button").length;
+console.log(length);
 
-for(let i = 0; i <length; i++){
-    $('.tab-button').eq(i).on('click',function(){
-        $('.tab-button').removeClass('orange');
-        $('.tab-button').eq(i).addClass('orange');
-        $('.tab-content').removeClass('show');
-        $('.tab-content').eq(i).addClass('show');
-    })
+// for(let i = 0; i <length; i++){
+//     $('.tab-button').eq(i).on('click',function(){
+//         $('.tab-button').removeClass('orange');
+//         $('.tab-button').eq(i).addClass('orange');
+//         $('.tab-content').removeClass('show');
+//         $('.tab-content').eq(i).addClass('show');
+//     })
+// }
+
+// 함수록 축약하기
+// for (let i = 0; i < length; i++) {
+//   $(".tab-button")
+//     .eq(i)
+//     .on("click", function () {
+//         탭열기(i)
+//     });
+// }
+
+$(".list").click(function (e) {
+  // 지금 누른게 버튼0이면 탭열기(0)
+  // 지금 누른게 버튼1이면 탭열기(1)
+  // 지금 누른게 버튼2이면 탭열기(2)
+
+  //   if (e.target == document.querySelectorAll(".tab-button")[0]) {
+  //     탭열기(0);
+  //   }
+  //   if (e.target == document.querySelectorAll(".tab-button")[1]) {
+  //     탭열기(1);
+  //   }
+  //   if (e.target == document.querySelectorAll(".tab-button")[2]) {
+  //     탭열기(2);
+  //   }
+ console.log(e.target.dataset.id);
+  탭열기(e.target.dataset.id);
+});
+
+function 탭열기(숫자) {
+  $(".tab-button").removeClass("orange");
+  $(".tab-button").eq(숫자).addClass("orange");
+  $(".tab-content").removeClass("show");
+  $(".tab-content").eq(숫자).addClass("show");
 }
-
-
-
-
-
-
-
-
-
-
